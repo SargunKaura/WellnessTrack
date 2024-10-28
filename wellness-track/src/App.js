04fade9d9fdd62b1099a0e004ab02d6229c55714
SargunKaura <kaurasargun@gmail.com>
2024-10-28 16:10:50 -0700

    Backend

--- wellness-track/src/App.js
+++ wellness-track/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/Home/HomePage.js";
 import LoginPage from "./components/Login/LoginPage.js";
-import RegisterPage from "./components/Register/RegisterPage.js";
+import RegisterPage from "./components/Patient/RegisterPage.js";
 import DashboardPage from './components/Dashboard/DashboardPage.js';
 import PatientProfile from './components/Patient/PatientProfile.js';
 import PatientResults from './components/Patient/PatientResults.js';
